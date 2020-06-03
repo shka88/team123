f02232ee8f7b0e541c3e8a9c665da6a945dd4f6b
KimJisnsu66 <jinsu6688@gmail.com>
2020-06-03 12:22:43 +0900

    MyApp 클래스 주석문 수정 Aquarium 클래스 소스코드 수정

--- Aquarium.java
+++ Aquarium.java
@@ -2,18 +2,18 @@
 /**
  * Aquarium 입장료를 계산해주는 메소드를 포함한 Aquarium 클래스
  * 
- * @author (2020.05.29) 
+ * @author (2020.06.03) 
  * @version (2014671038 김진수, 2018315030 이가영, 2018315053 테라오카 유이카 )
  */
 public class Aquarium
 {
     /**calculate 나이를 입력하면 해당 나이에 맞는 요금을 return 해준다.
-     * @param age 나이를 입력한다.
+     * @param age 나이를 입력값으로 전달받는다. 
      * @return 나이에 맞는 요금을 return 해준다.  
      */
     public int calculate(int age){
         int money = 0 ;
-        if(age<=150 && age >= 16){
+        if(age >= 16){
             money = 2000;
         }
         else if (age>=7){
--- MyApp.java
+++ MyApp.java
@@ -1,6 +1,6 @@
 import java.util.*;
 /**
- * Aquarium 입장료를 계산해주는 메소드를 포함한 Aquarium 클래스
+ * 요금을 출력해주는 main 메소드를 포함한 MyApp클래스 
  * 
  * @author (2020.06.03) 
  * @version (2014671038 김진수, 2018315030 이가영, 2018315053 테라오카 유이카 )
@@ -11,7 +11,7 @@ public class MyApp
         Scanner sc = new Scanner(System.in);
         System.out.print("나이를 입력하세요 : ");
         int age = sc.nextInt();
-        Aquarium aqualium = new Aquarium();
-        System.out.println(aqualium.calculate(age));
+        Aquarium aquarium = new Aquarium();
+        System.out.println("요금 : " + aquarium.calculate(age));
     }
 }
